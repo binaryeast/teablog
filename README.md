a368ca52d3c2aa19d6be83e0a0258ce1cf5e9d5b
binaryeast <binaryeast@gmail.com>
2021-03-14 10:56:45 +0000

    first model making

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Teablog is simple-short blog service.
 유어슈 백엔드 지원 과제입니다. 잘 부탁드립니다.
 
 기존의 WSL2 + docker 기반 로컬 개발 환경에서 MySQL이 설치가 진행되지 않아
-눈물을 머금고 AWS bitnami 이미지에서 새로 개발을 진행하는 브랜치가 main입니다.
+눈물을 머금고 AWS bitnami 이미지에서 새로 개발을 진행하는 브랜치가 master 입니다.
 
 cookiecutter-django라는 보일러플레이트를 못 쓰는 점이 가장 큰 차이점입니다.
 
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class TimeStampModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class TeaBlog(models.Model):
+    title = models.CharField(max_length=500)
+    owner = models.ForeignKey(User, verbose_name="개최자", on_delete=models.CASCADE)
+    open_status = [
+            ("PUB", "Public"),
+            ("PRV", "Private")
+            ]
+    is_public = models.CharField(
+            "공개 상태",
+            max_length = 3,
+            choices = open_status,
+            default = open_status[0][0]
+            )
+
