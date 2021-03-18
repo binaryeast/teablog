82e1c3fb63859f292a4113401f66cecf829d20cb
binaryeast <binaryeast@gmail.com>
2021-03-18 19:18:32 +0900

    import viewsets

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions, renderers, viewsets
 
 from .models import TeaBlog, TeaPost, Comment
 from .serializers import UserSerializer, TeaBlogSerializer, TeaPostSerializer, CommentSerializer
